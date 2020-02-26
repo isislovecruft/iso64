46dfab776c06b18f13cddfabe9936edf31aa647f
Isis Lovecruft <isis@patternsinthevoid.net>
2020-02-26 20:50:34 +0000

    Them's the rules and shit.

--- src/c64/main.asm
+++ src/c64/main.asm
@@ -1,7 +1,16 @@
-	;; Supersingular-isogeny key encapsulation in 6502/6510 assembly
+;; -*- mode: asm -*-
+;;
+;; This file is part of a Supersingular Isogeny Key Encapsulation (SIKE) over P434 for Commodore 64.
+;; Copyright (c) 2020 isis lovecruft
+;; See LICENSE for licensing information.
+;;
+;; Authors:
+;; - isis agora lovecruft <isis@patternsinthevoid.net>
+	
+;; Supersingular-isogeny key encapsulation over the P434 Galois field in 6502/6510 assembly for Commodore 64.
 
 !cpu 6510 	                    ; For 6502/6510 with undocumented opcodes
-!zone sike
+!zone sike                      ; Namespacing
 !to "sike", cbm                 ; Direct the assembler to output to this file, and store a c64-style program counter
 * = $cf00                       ; Set the program counter
 
